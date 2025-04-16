c80d34b8ec0ac1d522f3f5aa3aabc54e9325be0d
erwan <erwan.a.marechal@gmail.com>
2025-04-16 15:04:36 +0200

    Refactor App component for improved structure and clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react"
 import GameSetup from "./components/GameSetup"
-import GameBoard from "./components/GameBoard"
+import GameBoard from "./components/GameBoard" // <- ton écran principal avec la grille
+
 function App() {
   const [started, setStarted] = useState(false)
 
-  return started ? <GameBoard /> : <GameSetup onStart={() => setStarted(true)} />
+  return (
+    <>
+      {started ? (
+        <GameBoard />
+      ) : (
+        <GameSetup onStart={() => setStarted(true)} />
+      )}
+    </>
+  )
 }
 
 export default App
